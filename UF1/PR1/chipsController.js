4c6866d97229787bf2850d3a95c94cf1802b3851
XxArnauGxX <gilgilarnau@gmail.com>
2025-04-09 19:44:22 +0200

    añadido cookies y estilos a la PR1

--- UF1/PR1/chipsController.js
+++ UF1/PR1/chipsController.js
@@ -1,3 +1,28 @@
 const contadorChips = document.getElementById('chips');
+// add chips
+const addChipsBtn = document.getElementById('add-chips-btn');
+const addChipsInput = document.getElementById('add-chips');
+
 let chipsValue = 0;
-document.cookie = `chips={chipsValue}`
+document.cookie = `chips=${chipsValue};`;
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    addChipsBtn.addEventListener('click', () => {
+        if (addChipsInput.value <= 0) {
+            alert('El valor a añadir no puede ser negativo o 0');
+        } else {
+            alert(`¡Has añadido: ${addChipsInput.value} fichas!`);
+            chipsValue = parseInt(chipsValue) + parseInt(addChipsInput.value);
+            contadorChips.textContent = `${chipsValue}`;
+        }
+    })
+});
+
+function getCookie(name) {
+
+}
+
+function setCookie(name, value) {
+
+}
